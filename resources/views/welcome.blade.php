9ecc1384a0f2d8943980bf84eefb45f23e4d36d5
Luke <lukevandepol@gmail.com>
2023-04-19 11:53:53 +0200

    Vite + Composer aanpassingen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,3 @@
 <x-layout :title="'- Home'">
-    
+    <strong>Database Connected: </strong> <?php try { \DB::connection()->getPDO(); echo \DB::connection()->getDatabaseName(); } catch (\Exception $e) { echo 'None'; } ?>
 </x-layout>
