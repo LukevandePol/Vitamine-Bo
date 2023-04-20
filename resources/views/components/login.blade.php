d0f1fbc0e9d3d5c2310999967275715d04e01438
bokolmeijer <bokolmeijer@gmail.com>
2023-04-20 13:32:07 +0200

    login scherm basis

---
+++ resources/views/components/login.blade.php
@@ -0,0 +1,29 @@
+<main class="form-signin w-100 m-auto">
+    <form>
+
+        <div class="card mx-auto" style="width: 25rem;">
+    <div class="card-body">
+        <img src="/images/Logo.PNG" class="mb-4" alt="Logo" width="72" height="57">
+
+        <h1 class="h3 mb-3 fw-normal">Log in</h1>
+
+        <div class="form-label">
+            <label for="firstName" class="form-label">KvK-Nummer</label>
+            <input type="text" class="form-control" id="floatingInput">
+        </div>
+        <div class="form-label">
+            <label for="firstName" class="form-label">Wachtwoord</label>
+            <input type="password" class="form-control" id="floatingPassword">
+        </div>
+
+        <div class="checkbox mb-3">
+            <label>
+                <input type="checkbox" value="remember-me"> Onthoud mij
+            </label>
+        </div>
+        <button class="button-login" type="submit">Inloggen</button>
+    </div>
+</div>
+
+    </form>
+</main>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
 <x-layout :title="'- Home'">
     <x-login></x-login>
-    <x-header></x-header>
+
 </x-layout>
