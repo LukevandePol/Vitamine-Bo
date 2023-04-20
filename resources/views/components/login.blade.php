2e0c082625002ae5f1eec8baed8e6bbdd7553424
bokolmeijer <bokolmeijer@gmail.com>
2023-04-20 14:08:16 +0200

    aanpassingen van labels en login scherm

--- resources/views/components/login.blade.php
+++ resources/views/components/login.blade.php
@@ -1,19 +1,20 @@
 <main class="form-signin w-100 m-auto">
     <form>
+        <div class="logo center"> <img src="/images/Logo.PNG" alt="Logo" width="82" height="67"></div>
 
         <div class="card mx-auto" style="width: 25rem;">
     <div class="card-body">
-        <img src="/images/Logo.PNG" class="mb-4" alt="Logo" width="72" height="57">
+
 
         <h1 class="h3 mb-3 fw-normal">Log in</h1>
 
         <div class="form-label">
-            <label for="firstName" class="form-label">KvK-Nummer</label>
-            <input type="text" class="form-control" id="floatingInput">
+            <label for="email" class="form-label">E-mail</label>
+            <input type="text" class="form-control" id="email"> <br>
         </div>
         <div class="form-label">
-            <label for="firstName" class="form-label">Wachtwoord</label>
-            <input type="password" class="form-control" id="floatingPassword">
+            <label for="Wachtwoord" class="form-label">Wachtwoord</label>
+            <input type="password" class="form-control" id="Wachtwoord">
         </div>
 
         <div class="checkbox mb-3">
@@ -21,7 +22,8 @@
                 <input type="checkbox" value="remember-me"> Onthoud mij
             </label>
         </div>
-        <button class="button-login" type="submit">Inloggen</button>
+
+       <div class="center w-100"> <button class="button-login center" type="submit">Inloggen</button> </div>
     </div>
 </div>
 
