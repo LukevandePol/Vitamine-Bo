65242ab7e13ecaaecdbf1cf7490dc12e732d6a97
dionpool <elirionn@gmail.com>
2023-04-24 11:39:14 +0200

    Header class refactor

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,9 +1,9 @@
-<div class="banner">
+<header>
     <svg id="offset-triangle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 450.5">
         <polygon points="210 290.5 0 229.5 0 0 12520 0" fill="#FEFEFE" stroke="#F6F6F6" stroke-width="15" filter="url(#dropshadow)"/>
     </svg>
-    <div class="picture-container">
-        <img src="/images/Logo.PNG" class="img-fluid" alt="logo" width="100%">
+    <div class="logo-wrapper">
+        <img src="/images/Logo.PNG" class="img-fluid w-100" alt="logo">
     </div>
-</div>
+</header>
 
