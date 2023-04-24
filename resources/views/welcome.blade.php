c8c974b525e5564c5e4044125e3be34832330025
dionpool <elirionn@gmail.com>
2023-04-24 11:42:23 +0200

    Voegt buttons en links toe

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,11 @@
 <x-layout :title="'- Home'">
-    <x-login></x-login>
+    <form method="POST" action="/uitloggen">
+        @csrf
+        @auth
+            <button type="submit">Log Out</button>
+        @else
+            <a href="/inloggen">Inloggen</a>
+            <a href="/registreren">Registreren</a>
+        @endauth
+    </form>
 </x-layout>
