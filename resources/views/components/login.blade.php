73db483386fa6d8ff4ce0c70fc2841aa085aa467
dionpool <elirionn@gmail.com>
2023-04-24 11:39:42 +0200

    Voegt login pagina

--- resources/views/components/login.blade.php
+++ resources/views/components/login.blade.php
@@ -1,31 +1,28 @@
 <main class="form-signin w-100 m-auto">
-    <form>
-        <div class="logo center"> <img src="/images/Logo.PNG" alt="Logo" width="82" height="67"></div>
+    <form method="POST" action="/inloggen">
+        @csrf
 
-        <div class="card mx-auto" style="width: 25rem;">
-    <div class="card-body">
+        <x-card>
+            <h1 class="h3 mb-3 fw-normal">Log in</h1>
 
+            <div class="form-label">
+                <label for="email" class="form-label">E-mail</label>
+                <input type="text" class="form-control" id="email">
+            </div>
 
-        <h1 class="h3 mb-3 fw-normal">Log in</h1>
+            <div class="form-label">
+                <label for="Wachtwoord" class="form-label">Wachtwoord</label>
+                <input type="password" class="form-control" id="Wachtwoord">
+            </div>
 
-        <div class="form-label">
-            <label for="email" class="form-label">E-mail</label>
-            <input type="text" class="form-control" id="email"> <br>
-        </div>
-        <div class="form-label">
-            <label for="Wachtwoord" class="form-label">Wachtwoord</label>
-            <input type="password" class="form-control" id="Wachtwoord">
-        </div>
+            <div class="checkbox mb-3">
+                <input type="checkbox" name="remember" id="remember">
+                <label for="remember">Onthoud mij</label>
+            </div>
 
-        <div class="checkbox mb-3">
-            <label>
-                <input type="checkbox" value="remember-me"> Onthoud mij
-            </label>
-        </div>
-
-       <div class="center w-100"> <button class="button-login center" type="submit">Inloggen</button> </div>
-    </div>
-</div>
-
+            <div class="center w-100">
+                <button class="button-login center" type="submit">Inloggen</button>
+            </div>
+        </x-card>
     </form>
 </main>
