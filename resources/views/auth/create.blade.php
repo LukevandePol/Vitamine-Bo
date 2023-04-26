dac7f255a0c819c6b9f230a8f7e43bf8a8771a98
dionpool <elirionn@gmail.com>
2023-04-26 14:47:36 +0200

    Voegt registeren/inloggen linkjes toe
    Signed-off-by: dionpool <elirionn@gmail.com>

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -10,15 +10,18 @@
             <form method="POST" action="/registreren">
                 @csrf
 
-                <x-input label="Naam:" name="name" />
-                <x-input label="Wachtwoord:" type="password" name="password" />
-                <x-input label="KvK nummer:" type="number" name="kvknummer" placeholder="8 cijfers" />
-                <x-input label="Adres:" name="adres" />
-                <x-input label="Telefoonnummer:" type="number" name="telefoon" />
-                <x-input label="Postcode:" name="postcode" />
-                <x-input label="E-mailadres:" type="email" name="email" />
+                <x-input label="Naam:" name="name"/>
+                <x-input label="Wachtwoord:" type="password" name="password"/>
+                <x-input label="KvK nummer:" type="number" name="kvknummer" placeholder="8 cijfers"/>
+                <x-input label="Adres:" name="adres"/>
+                <x-input label="Telefoonnummer:" type="number" name="telefoon"/>
+                <x-input label="Postcode:" name="postcode"/>
+                <x-input label="E-mailadres:" type="email" name="email"/>
 
-                <x-submit>Registreren</x-submit>
+                <div class="d-flex align-items-center justify-content-between">
+                    <x-submit>Registreren</x-submit>
+                    <a href="inloggen">Al een account? Log in</a>
+                </div>
             </form>
         </x-card>
     </main>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,15 +10,19 @@
             <form method="POST" action="/inloggen">
                 @csrf
 
-                <x-input label="E-mailadres:" type="email" name="email" />
-                <x-input label="Wachtwoord:" type="password" name="password" />
+                <x-input label="E-mailadres:" type="email" name="email"/>
+                <x-input label="Wachtwoord:" type="password" name="password"/>
                 <div class="form-check mb-3">
                     <input class="form-check-input" type="checkbox" id="remember" name="remember">
                     <label class="form-check-label" for="remember">
                         Onthoud mij
                     </label>
                 </div>
-                <x-submit>Inloggen</x-submit>
+
+                <div class="d-flex align-items-center justify-content-between">
+                    <x-submit>Inloggen</x-submit>
+                    <a href="registreren">Nog geen account? Registreer</a>
+                </div>
             </form>
         </x-card>
     </main>
