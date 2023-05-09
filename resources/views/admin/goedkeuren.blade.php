f48bfa9be95306d37a8b31e5cc013e65065d63b2
dionpool <elirionn@gmail.com>
2023-05-09 11:31:19 +0200

    Laat alle niet goedgekeurde gebruikers zien
    Signed-off-by: dionpool <elirionn@gmail.com>

---
+++ resources/views/admin/goedkeuren.blade.php
@@ -0,0 +1,22 @@
+<x-layout>
+    <div class="container py-5">
+        <h1>Goedkeuren</h1>
+        <table class="table mt-5">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Naam</th>
+                <th scope="col">Actie</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($users as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</x-layout>
