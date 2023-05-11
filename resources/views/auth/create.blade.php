afb33fb35ae598616808ae9604be96150f16f087
dionpool <elirionn@gmail.com>
2023-05-11 14:24:44 +0200

    Voegt design voor registratie form toe
    Signed-off-by: dionpool <elirionn@gmail.com>

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -1,28 +1,46 @@
 <x-layout title="- Registreren">
-    <main class="w-100">
-        <x-card>
-            <div class="center">
-                <x-logo/>
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-lg-6">
+                <img src="https://picsum.photos/448/704" class="rounded shadow" alt="placeholder">
             </div>
-
-            <h1 class="h3 mb-3 fw-normal">Registreren</h1>
-
-            <form method="POST" action="/registreren">
-                @csrf
-
-                <x-input label="Naam:" name="name"/>
-                <x-input label="Wachtwoord:" type="password" name="password"/>
-                <x-input label="KvK nummer:" type="number" name="kvknummer" placeholder="8 cijfers"/>
-                <x-input label="Adres:" name="adres"/>
-                <x-input label="Telefoonnummer:" type="number" name="telefoon"/>
-                <x-input label="Postcode:" name="postcode"/>
-                <x-input label="E-mailadres:" type="email" name="email"/>
-
-                <div class="d-flex align-items-center justify-content-between">
-                    <x-submit>Registreren</x-submit>
-                    <a href="inloggen">Al een account? Log in</a>
+            <div class="col-lg-6 my-auto">
+                <div class="d-flex justify-content-center align-items-center flex-column mb-5">
+                    <x-logo width="120"/>
+                    <div class="tab-status my-5">
+                        <span class="tab active"></span>
+                        <span class="tab"></span>
+                        <span class="tab"></span>
+                    </div>
+                    <h1 class="h2">Registreren</h1>
                 </div>
-            </form>
-        </x-card>
-    </main>
+
+                <form method="POST" action="/registreren">
+                    @csrf
+
+                    <x-tabpanel id="login">
+                        <x-input label="E-mailadres" type="email" name="email"/>
+                        <x-input label="Telefoonnummer:" type="tel" name="telefoon"/>
+                        <x-input label="Wachtwoord:" type="password" name="password"/>
+                    </x-tabpanel>
+
+                    <x-tabpanel id="bedrijf" class="hidden">
+                        <x-input label="KVK-nummer:" name="kvknummer"/>
+                        <x-input label="Bedrijfsnaam:" name="name"/>
+                    </x-tabpanel>
+
+                    <x-tabpanel id="adres" class="hidden">
+                        <x-input label="Adres:" name="adres"/>
+                        <x-input label="Postcode:" name="postcode"/>
+                    </x-tabpanel>
+
+                    <div class="pagination">
+                        <x-button class="btn-primary hidden" id="prev">Vorige</x-button>
+                        <x-button class="btn-primary" id="next">Volgende</x-button>
+                        <x-submit class="btn btn-primary hidden" id="submit">Registreren</x-submit>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </x-layout>
