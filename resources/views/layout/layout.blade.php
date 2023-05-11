3ef5d7be142ff84236e40e3d833161521dc958c4
dionpool <elirionn@gmail.com>
2023-05-11 14:25:17 +0200

    Voegt link toe in header
    Signed-off-by: dionpool <elirionn@gmail.com>

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -25,9 +25,11 @@
             </a>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/">Home</a>
-                    </li>
+                    @auth
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Home</a>
+                        </li>
+                    @endauth
                     @can('isAdmin')
                         <li class="nav-item">
                             <a class="nav-link" href="/admin">Dashboard</a>
