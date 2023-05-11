18e363103c2b48023302e26f93120c8903d9335c
bokolmeijer <bokolmeijer@gmail.com>
2023-05-11 14:34:06 +0200

    Cards maken voor de content

---
+++ resources/views/components/cardstripe.blade.php
@@ -0,0 +1,7 @@
+<div {{ $attributes->merge(['class' => 'card-custom']) }}>
+    <h5 class="card-title">
+        Card Title
+    </h5>
+    <div style="border-bottom: 3px solid #B2DFF5; padding-bottom: 7px;"></div>
+    <p class="card-text">Card content goes here</p>
+</div>
