d1a55d2e51fcb855fce9a7cdab640e06f322461b
Luke <lukevandepol@gmail.com>
2023-05-16 11:43:00 +0200

    font voor h2,h3 en dashboard begin
    Signed-off-by: Luke <lukevandepol@gmail.com>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,19 +3,26 @@
 
         <div class="row">
             <div class="col-12">
-                <div class="welkom-banner">
-                    <h1>Welkom Bam</h1>
+                <div class="welkom-banner padding-left">
+                    <h1 class="welkom-header">Welkom Bam</h1>
                 </div>
             </div>
         </div>
 
-        <div class="row">
-            <div class="col-6">
+        <div class="row padding-left">
+            <div class="col-12">
+                <h2>Dashboard</h2>
+                <p>Hier zie je een overzicht van relevante informatie.</p>
+            </div>
+        </div>
+
+        <div class="row padding-left">
+            <div class="col-sm-6">
                 <x-card>
                     aankomende levering
                 </x-card>
             </div>
-            <div class="col-6">
+            <div class="col-sm-6">
                 <x-card>
                     goed keuring aanpassing
                 </x-card>
@@ -28,15 +35,17 @@
             </div>
         </div>
 
-        <div class="row">
+        <div class="row padding-left">
             <div class="col-12">
-                <h2> geef je werknemers een boost</h2>
+                <h2>Geef je werknemers een boost</h2>
                 <p> Voeg ook onze heerlijke pakketen toe</p>
             </div>
         </div>
 
-        <div class="row">
-            <div class="col-12">Recente bestellingen</div>
+        <div class="row padding-left">
+            <div class="col-12">
+                <h2>Recente bestellingen</h2>
+            </div>
         </div>
     </div>
 </x-layout>
