60d95774c90e15e47d5e03da006363acaa6e37f6
Luke <lukevandepol@gmail.com>
2023-05-16 12:33:49 +0200

    dashboard link toevoegen
    Signed-off-by: Luke <lukevandepol@gmail.com>

--- resources/views/layout/navigation.blade.php
+++ resources/views/layout/navigation.blade.php
@@ -12,6 +12,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="/account">Account</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/dashboard">Dashboard</a>
+                    </li>
                 @endauth
                 @can('isAdmin')
                     <li class="nav-item">
