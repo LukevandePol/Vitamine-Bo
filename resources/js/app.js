1772edd3be00ec213695fba1f79c4e58623de3d1
bokolmeijer <bokolmeijer@gmail.com>
2023-05-16 13:51:45 +0200

    sidebar toevoegen

--- resources/js/app.js
+++ resources/js/app.js
@@ -55,7 +55,6 @@ function updateStatusDisplay() {
 //sidebar//
 let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bx-search");
 
 closeBtn.addEventListener("click", ()=>{
     sidebar.classList.toggle("open");
