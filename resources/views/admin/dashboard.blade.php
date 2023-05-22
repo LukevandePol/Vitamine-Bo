ee506852e490f5c01d594e84a520cceaf024e028
dionpool <elirionn@gmail.com>
2023-05-22 14:13:01 +0200

    Refactor Admin Panel
    Signed-off-by: dionpool <elirionn@gmail.com>

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-layout title="- Dashboard">
+<x-layout title="Dashboard" header="Admin Panel">
     <div class="container py-5">
         <span class="h3">Welkom {{ ucwords(auth()->user()->name) }}</span>
     </div>
