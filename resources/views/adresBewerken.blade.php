8fc06ed1e5a470e673d776c1db4685ae089906f3
Luke <lukevandepol@gmail.com>
2023-05-22 14:07:55 +0200

    rows en col toevoegen
    Signed-off-by: Luke <lukevandepol@gmail.com>

--- resources/views/adresBewerken.blade.php
+++ resources/views/adresBewerken.blade.php
@@ -1,23 +1,29 @@
 <x-layout :title="'- Adres bewerken'">
-    {{$adres}}
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                {{$adres}}
 
-    <form action="/updateAdres/{{$adres->id}}" method="post">
-        @csrf
-        <x-input
-            label="Postcode: "
-            name="postcode"
-            value="{{$adres->postcode}}"
-        />
-        <x-input
-            label="Huisnummer: "
-            name="huisnummer"
-            value="{{$adres->huisnummer}}"
-        />
-        <x-input
-            label="Plaatsnaam"
-            name="plaatsnaam"
-            value="{{$adres->plaatsnaam}}"
-        />
-        <x-submit>pas aan</x-submit>
-    </form>
+                <form action="/updateAdres/{{$adres->id}}" method="post">
+                    @csrf
+                    <x-input
+                        label="Postcode: "
+                        name="postcode"
+                        value="{{$adres->postcode}}"
+                    />
+                    <x-input
+                        label="Huisnummer: "
+                        name="huisnummer"
+                        value="{{$adres->huisnummer}}"
+                    />
+                    <x-input
+                        label="Plaatsnaam"
+                        name="plaatsnaam"
+                        value="{{$adres->plaatsnaam}}"
+                    />
+                    <x-submit>pas aan</x-submit>
+                </form>
+            </div>
+        </div>
+    </div>
 </x-layout>
