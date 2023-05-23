bd1c9040a1be5e38b3d2eec0aec2b0468c56f13e
dionpool <elirionn@gmail.com>
2023-05-23 12:39:45 +0200

    Dit is een layout
    Signed-off-by: dionpool <elirionn@gmail.com>

--- resources/views/admin/product.blade.php
+++ resources/views/admin/product.blade.php
@@ -1,3 +1,5 @@
-<x-layout title="Product">
-    test
+<x-layout title="Product" header="Producten" beschrijving="Voeg hier nieuwe producten toe of verwijder ze.">
+    <div class="container">
+
+    </div>
 </x-layout>
