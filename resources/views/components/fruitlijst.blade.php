66f21106584621c8c16f0b40b701b29a886e5cf2
bokolmeijer <bokolmeijer@gmail.com>
2023-05-23 10:36:09 +0200

    fruitlijst toevoegen

---
+++ resources/views/components/fruitlijst.blade.php
@@ -0,0 +1,124 @@
+
+<div class="container scrollable-container">
+    <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+            <img
+                src="images/Product2.png"
+                alt="fruit"
+                class="img-fluid shadowimg">
+        </div>
+    </div>
+</div>
+
