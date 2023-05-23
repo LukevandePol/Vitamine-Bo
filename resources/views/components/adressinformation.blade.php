545b8c5a0c95a4b9ba337ba7eb697c681ff6abf2
bokolmeijer <bokolmeijer@gmail.com>
2023-05-23 13:01:19 +0200

    adress card maken

--- resources/views/components/adressinformation.blade.php
+++ resources/views/components/adressinformation.blade.php
@@ -2,25 +2,25 @@
     <form>
         <div class="row">
             <div class="col-12 ">
-                <label for="street">Bedrijfsnaam:</label>
+                <label for="company">Bedrijfsnaam:</label>
                 <input type="text" id="company" name="company" class="form-control" required>
             </div>
         </div>
         <div class="row">
             <div class="col-12 my-3">
-                <label for="street">KvK-nummer:</label>
+                <label for="Kvk">KvK-nummer:</label>
                 <input type="number" id="KvK" name="KvK" class="form-control" required>
             </div>
         </div>
         <div class="row">
             <div class="col-12">
-                <label for="street">Email:</label>
+                <label for="email">Email:</label>
                 <input type="text" id="email" name="email" class="form-control" required>
             </div>
         </div>
         <div class="row">
             <div class="col-12 my-3">
-                <label for="street">Telefoonnummer:</label>
+                <label for="phone">Telefoonnummer:</label>
                 <input type="number" id="phone" name="phone" class="form-control" required>
             </div>
         </div>
