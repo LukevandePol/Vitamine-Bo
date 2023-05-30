1ae55b03629026452c4cfdd129babbe45af0c37f
dionpool <elirionn@gmail.com>
2023-05-30 12:20:29 +0200

    Sidebar fix
    Signed-off-by: dionpool <elirionn@gmail.com>

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,30 +1,5 @@
 import './bootstrap';
 
-// Sidebar
-let sidebar = document.querySelector(".sidebar");
-let closeBtn = document.querySelector("#btn");
-
-sidebar.addEventListener("mouseenter", () => {
-    sidebar.classList.add("open");
-    sidebar.style.width = "200px";
-    menuBtnChange(); // calling the function (optional)
-});
-
-sidebar.addEventListener("mouseleave", () => {
-    sidebar.classList.remove("open");
-    sidebar.style.width = "78px";
-    menuBtnChange(); // calling the function (optional)
-});
-
-// following are the code to change sidebar button(optional)
-function menuBtnChange() {
-    if(sidebar.classList.contains("open")){
-        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
-    }else {
-        closeBtn.classList.replace("bx-menu-alt-right","bx-menu");
-    }
-}
-
 // Carousel
 // const carousel = document.querySelector(".carousel"),
 //     firstImg = carousel.querySelectorAll(".carousel-img")[0],
