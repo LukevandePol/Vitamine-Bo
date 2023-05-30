7c41e9c170f39e1f1382c183512149e70a213b05
dionpool <elirionn@gmail.com>
2023-05-30 15:00:48 +0200

    Verwijderd niet gebruikte classes
    Signed-off-by: dionpool <elirionn@gmail.com>

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -3,12 +3,11 @@
         <i id="left" class="fa-solid fa-angle-left"></i>
         <div class="carousel">
             <a href="" title="Product 1">
-                <img
-                    src="{{ asset('images/Product1.png')}}"
-                    alt="img"
-                    draggable="false"
-                    class="carousel-img"
-                    style="margin-right: 10px;">
+                <img src="{{ asset('images/Product1.png')}}"
+                     alt="img"
+                     draggable="false"
+                     class="carousel-img"
+                     style="margin-right: 10px;">
             </a>
             <a href="" title="Product 2">
                 <img src="images/Product2.png"
@@ -18,21 +17,19 @@
                      style="margin-right: 10px;">
             </a>
             <a href="" title="Product 3">
-                <img
-                    src="images/Product3.png"
-                    alt="img"
-                    class="carousel-img"
-                    draggable="false"
-                    style="margin-right: 10px;">
+                <img src="images/Product3.png"
+                     alt="img"
+                     class="carousel-img"
+                     draggable="false"
+                     style="margin-right: 10px;">
             </a>
             <a
                 href="" title="Product 4">
-                <img
-                    src="images/Product4.png"
-                    alt="img"
-                    class="carousel-img"
-                    draggable="false"
-                    style="margin-right: 10px;">
+                <img src="images/Product4.png"
+                     alt="img"
+                     class="carousel-img"
+                     draggable="false"
+                     style="margin-right: 10px;">
             </a>
             <a href="" title="Product 5">
                 <img src="images/Product5.png"
