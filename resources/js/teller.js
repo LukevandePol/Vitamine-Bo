88bb68eac59cd7561e29e26539538533616a4673
dionpool <elirionn@gmail.com>
2023-05-31 11:25:57 +0200

    Voegt teller javascript toe
    Signed-off-by: dionpool <elirionn@gmail.com>

--- resources/js/teller.js
+++ resources/js/teller.js
@@ -1,30 +1,51 @@
-window.onload = () => {
+document.addEventListener("DOMContentLoaded", () => {
     const wrappers = document.querySelectorAll(".wrapper");
+    const totalCounter = document.querySelector("#total-counter");
+    const minusButtons = document.querySelectorAll(".minus");
+    const plusButtons = document.querySelectorAll(".plus");
+    const numSpans = document.querySelectorAll(".num");
+    let totalCount = 0;
 
-    wrappers.forEach((wrapper) => {
-        const minus = wrapper.querySelector(".minus");
-        const plus = wrapper.querySelector(".plus");
-        const num = wrapper.querySelector(".num");
-        let a = 1;
+    // Loop to calculate initial total count
+    numSpans.forEach((num) => {
+        totalCount += parseInt(num.innerText);
+    });
+
+    // Update count and total counter for each wrapper
+    wrappers.forEach((wrapper, index) => {
+        const minus = minusButtons[index];
+        const plus = plusButtons[index];
+        const num = numSpans[index];
+        let count = parseInt(num.innerText); // Initialize count based on the initial value
 
         plus.addEventListener("click", () => {
-            a++;
-            if (a < 10) {
-                num.innerText = a;
-            } else {
-                num.innerText = a;
+            if (totalCount < 30 && count < 30) {
+                count++;
+                num.innerText = count;
+                updateTotal();
             }
         });
 
         minus.addEventListener("click", () => {
-            if (a > 1) {
-                a--;
-                if (a < 10) {
-                    num.innerText = a;
-                } else {
-                    num.innerText = a;
-                }
+            if (count > 0) {
+                count--;
+                num.innerText = count;
+                updateTotal();
             }
         });
     });
-};
+
+    function updateTotal() {
+        totalCount = Array.from(numSpans).reduce((sum, span) => sum + parseInt(span.innerText), 0);
+        totalCounter.innerText = `${totalCount} / 30`;
+
+        if (totalCount >= 30) {
+            totalCounter.style.color = "red";
+        } else {
+            totalCounter.style.color = "initial";
+        }
+    }
+
+    // Initialize the total count
+    updateTotal();
+});
