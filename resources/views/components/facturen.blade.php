9b6eb6d8c4604814eefcd708396b525c2f7b4d04
bokolmeijer <bokolmeijer@gmail.com>
2023-06-05 13:29:27 +0200

    facturen data toevoegen

---
+++ resources/views/components/facturen.blade.php
@@ -0,0 +1,138 @@
+<x-card>
+<h2>Facturen</h2>
+    <p>Hieronder vind je een overicht van jouw facturen per jaar.</p>
+<div class="table-responsive">
+    <table class="table table-striped table-sm">
+        <thead>
+        <tr>
+            <th scope="col">Maand</th>
+            <th scope="col">Factuur nr.</th>
+            <th scope="col">Afnemer</th>
+            <th scope="col">Betaald</th>
+            <th scope="col">Pdf</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Mei</td>
+            <td>19200009</td>
+            <td>Vitamine Bo B.V.</td>
+            <td><i class="fa-solid fa-check"></i></td>
+            <td><i class="fa-solid fa-download"></i></td>
+        </tr>
+        <tr>
+            <td>April</td>
+            <td>19200008</td>
+            <td>Vitamine Bo B.V.</td>
+            <td><i class="fa-solid fa-check"></i></td>
+            <td><i class="fa-solid fa-download"></i></td>
+        </tr>
+        <tr>
+            <td>Maart</td>
+            <td>19200007</td>
+            <td>Vitamine Bo B.V.</td>
+            <td><i class="fa-solid fa-check"></i></td>
+            <td><i class="fa-solid fa-download"></i></td>
+        </tr>
+        <tr>
+            <td>Februari</td>
+            <td>19200006</td>
+            <td>Vitamine Bo B.V.</td>
+            <td><i class="fa-solid fa-check"></i></td>
+            <td><i class="fa-solid fa-download"></i></td>
+        </tr>
+        <tr>
+            <td>Januari</td>
+            <td>19200005</td>
+            <td>Vitamine Bo B.V.</td>
+            <td><i class="fa-solid fa-check"></i></td>
+            <td><i class="fa-solid fa-download"></i></td>
+        </tr>
+        <tr>
+            <td>December</td>
+            <td>19200004</td>
+            <td>Vitamine Bo B.V.</td>
+            <td><i class="fa-solid fa-check"></i></td>
+            <td><i class="fa-solid fa-file-arrow-down"></i></td>
+        </tr>
+        <tr>
+            <td>November</td>
+            <td>19200003</td>
+            <td>Vitamine Bo B.V.</td>
+            <td><i class="fa-solid fa-check"></i></td>
+            <td><i class="fa-solid fa-download"></i></td>
+        </tr>
+        <tr>
+            <td>Oktober</td>
+            <td>19200002</td>
+            <td>Vitamine Bo B.V.</td>
+            <td><i class="fa-solid fa-check"></i></td>
+            <td><i class="fa-solid fa-download"></i></td>
+        </tr>
+        <tr>
+            <td>September</td>
+            <td>19200001</td>
+            <td>Vitamine Bo B.V.</td>
+            <td><i class="fa-solid fa-check"></i></td>
+            <td><i class="fa-solid fa-download"></i></td>
+        </tr>
+        <tr>
+            <td>Augustus</td>
+            <td>19200000</td>
+            <td>Vitamine Bo B.V.</td>
+            <td><i class="fa-solid fa-check"></i></td>
+            <td><i class="fa-solid fa-download"></i></td>
+        </tr>
+        <tr>
+            <td>Juli</td>
+            <td>19200099</td>
+            <td>Vitamine Bo B.V.</td>
+            <td><i class="fa-solid fa-check"></i></td>
+            <td><i class="fa-solid fa-download"></i></td>
+        </tr>
+        <tr>
+            <td>Juni</td>
+            <td>19200098</td>
+            <td>Vitamine Bo B.V.</td>
+            <td><i class="fa-solid fa-check"></i></td>
+            <td><i class="fa-solid fa-download"></i></td>
+        </tr>
+        <tr>
+            <td>Mei</td>
+            <td>19200097</td>
+            <td>Vitamine Bo B.V.</td>
+            <td><i class="fa-solid fa-check"></i></td>
+            <td><i class="fa-solid fa-download"></i></td>
+        </tr>
+        <tr>
+            <td>April</td>
+            <td>19200096</td>
+            <td>Vitamine Bo B.V.</td>
+            <td><i class="fa-solid fa-check"></i></td>
+            <td><i class="fa-solid fa-download"></i></td>
+        </tr>
+        <tr>
+            <td>Maart</td>
+            <td>19200095</td>
+            <td>Vitamine Bo B.V.</td>
+            <td><i class="fa-solid fa-check"></i></td>
+            <td><i class="fa-solid fa-download"></i></td>
+        </tr>
+        <tr>
+            <td>Februari</td>
+            <td>19200094</td>
+            <td>Vitamine Bo B.V.</td>
+            <td><i class="fa-solid fa-check"></i></td>
+            <td><i class="fa-solid fa-download"></i></td>
+        </tr>
+        <tr>
+            <td>Januari</td>
+            <td>192000093</td>
+            <td>Vitamine Bo B.V.</td>
+            <td><i class="fa-solid fa-check"></i></td>
+            <td><i class="fa-solid fa-download"></i></td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+</x-card>
