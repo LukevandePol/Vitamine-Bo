0442e1611a556f8c07df0409cc44940e8c11b269
bokolmeijer <bokolmeijer@gmail.com>
2023-06-05 19:40:46 +0200

    filter toevoegen

--- resources/views/components/facturen.blade.php
+++ resources/views/components/facturen.blade.php
@@ -1,5 +1,23 @@
 <x-card>
 <h2>Facturen</h2>
+    <div class="row">
+        <div class="col-md-6 mb-2">
+            <div class="d-flex align-items-center jaar-dropdown-container">
+                <p class="mb-0 me-2">Jaar</p>
+                <div class="dropdown">
+                    <button class="btn btn-white dropdown-toggle" type="button" id="yearDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="yearDropdown" id="yearDropdownMenu">
+                        <a class="dropdown-item" href="#" data-value="2023">2023</a>
+                        <a class="dropdown-item" href="#" data-value="2022">2022</a>
+                        <a class="dropdown-item" href="#" data-value="2021">2021</a>
+                        <a class="dropdown-item" href="#" data-value="2020">2020</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <p>Hieronder vind je een overicht van jouw facturen per jaar.</p>
 <div class="table-responsive">
     <table class="table table-striped table-sm">
