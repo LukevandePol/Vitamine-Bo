565bd9f41ecf85c027e410732511dbeddc6adb6a
bokolmeijer <bokolmeijer@gmail.com>
2023-06-07 14:27:55 +0200

    bestelling overzicht

--- resources/views/components.blade.php
+++ resources/views/components.blade.php
@@ -1,4 +1,116 @@
 <x-layout title="Components">
+    <div>
+    <x-cardstripe title="Huidige bestelling voor: 8 Mei" class="bo-hoofdkleur-opacity long-version">
+        <div class="margin-total-list">
+            <div class="item-row">
+                <h5>1x Mand (20 stuks)</h5>
+                <div class="icon-container">
+                    <i class="fa-regular fa-pen-to-square"></i>
+                    <i class="fa-solid fa-trash-can"></i>
+                </div>
+            </div>
+            <ul class="margin-ul-items">
+                <li>
+                    <span class="item-name">Appels</span>
+                    <span class="item-number-top">8</span>
+                </li>
+                <li>
+                    <span class="item-name">Bananen</span>
+                    <span class="item-number-top">5</span>
+                </li>
+                <li>
+                    <span class="item-name">Kiwi's</span>
+                    <span class="item-number-top">3</span>
+                </li>
+                <li>
+                    <span class="item-name">Mandarijen</span>
+                    <span class="item-number-top">4</span>
+                </li>
+            </ul>
+        </div>
+        <div class="margin-total-list mt-4">
+            <h5>Los Fruit</h5>
+            <ul class="margin-ul-items">
+                <li>
+                    <div class="item-row mb-1">
+                        <span class="item-name">Appels</span>
+                        <span class="item-number">
+                            <div class="icon-container">
+                               <i class="fa-solid fa-plus"></i>
+                             <span class="number">2</span>
+                            <i class="fa-solid fa-minus"></i>
+                            <i class="fa-solid fa-trash-can"></i>
+                            </div>
+                        </span>
+                    </div>
+                </li>
+                <li>
+                    <div class="item-row">
+                        <span class="item-name">Peren</span>
+                        <span class="item-number">
+                        <div class="icon-container">
+                            <i class="fa-solid fa-plus"></i>
+                             <span class="number">4</span>
+                            <i class="fa-solid fa-minus"></i>
+                            <i class="fa-solid fa-trash-can"></i>
+                            </div>
+                        </span>
+                    </div>
+                </li>
+            </ul>
+            <div class="button-bottom">
+                <x-button class="icon-button">Bevestigen</x-button>
+            </div>
+        </div>
+    </x-cardstripe>
+    </div>
 
+<div>
+    <x-cardstripe title="Huidige bestelling voor: 8 Mei" class="bo-hoofdkleur-opacity long-version">
+        <div class="margin-total-list">
+            <div class="item-row">
+                <h5>1x Mand (20 stuks)</h5>
+            </div>
+            <ul class="margin-ul-items">
+                <li>
+                    <span class="item-name">Appels</span>
+                    <span class="item-number-all">8</span>
+                </li>
+                <li>
+                    <span class="item-name">Bananen</span>
+                    <span class="item-number-all">5</span>
+                </li>
+                <li>
+                    <span class="item-name">Kiwi's</span>
+                    <span class="item-number-all">3</span>
+                </li>
+                <li>
+                    <span class="item-name">Mandarijen</span>
+                    <span class="item-number-all">4</span>
+                </li>
+            </ul>
+        </div>
+        <div class="margin-total-list mt-4">
+            <h5>Los Fruit</h5>
+            <ul class="margin-ul-items">
+                <li>
+                    <div class="item-row mb-1">
+                        <span class="item-name">Appels</span>
+                        <span class="item-number-all">4</span>
 
+                        </span>
+                    </div>
+                </li>
+                <li>
+                    <div class="item-row">
+                        <span class="item-name">Peren</span>
+                        <span class="item-number-all">2</span>
+
+                        </span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </x-cardstripe>
+    </div>
 </x-layout>
