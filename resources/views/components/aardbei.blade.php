3e8bbfc299c1c397326606290694fa4f0f9a3a65
dionpool <elirionn@gmail.com>
2023-06-08 09:59:50 +0200

    Voegt tijdelijk aardbei component toe
    Signed-off-by: dionpool <elirionn@gmail.com>

---
+++ resources/views/components/aardbei.blade.php
@@ -0,0 +1,27 @@
+<div class="dropup-container">
+    <button class="dropup-btn">
+        <img src="{{ asset('images/strawberry.svg') }}" class="aardbei" width="125" alt="Aardbei Hulp">
+    </button>
+    <div class="dropup-content">
+        <div class="accordion accordion-flush" id="faq">
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-link collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#faq-content">
+                        Title
+                    </button>
+                </h2>
+                <div id="faq-content" class="accordion-collapse collapse" data-bs-parent="#faq">
+                    <div class="accordion-body">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores nesciunt nostrum
+                        officiis placeat possimus vero? Accusamus aliquid, assumenda consequatur delectus
+                        exercitationem
+                        laboriosam modi molestiae molestias officia recusandae repudiandae saepe.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
