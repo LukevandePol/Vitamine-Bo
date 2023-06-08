85047572e9a7a9468bb0add877c942de23c97748
bokolmeijer <bokolmeijer@gmail.com>
2023-06-08 13:59:36 +0200

    placement

--- resources/views/admin/product-inhoud.blade.php
+++ resources/views/admin/product-inhoud.blade.php
@@ -1,42 +1,51 @@
 <x-layout title="Inhoud toevoegen">
     <div class="row">
-        <div class="col-12">
-            @if($inhoud !== null)
-                @foreach($inhoud as $item)
-                    <div style="border: 1px solid red">
-                        @php $product = \App\Models\Product::find($item->product_id)@endphp
-                        <p>{{$item->aantal}}</p>
-                        <p>{{$product->naam}}</p>
-                        <form action="/deleteVerpakkingInhoud" method="post">
-                            @csrf
-                            <input name="product_id" value="{{$product->id}}" type="hidden">
-                            <input name="selectie_id" value="{{$selectie->id}}" type="hidden">
-                            <x-submit>Verwijder</x-submit>
-                        </form>
+        <div class="col-8">
+            <div class="card-containerproduc">
+                @foreach(\App\Models\Product::all()->where('type', '=', 'fruit') as $product)
+                    <div class="mb-3">
+                        <x-cardstripe :title="$product->naam" class="font-stylecard">
+                            <form action="/verpakkingInhoudToevoegen" method="post">
+                                @csrf
+                                <x-input type="hidden" name="selectie_id" :value="$selectie->id" />
+                                <x-input type="hidden" name="product_id" :value="$product->id" />
+                                <div class="form-length">
+                                    <x-input name="aantal" label="aantal" />
+                                </div>
+                                <x-submit class="small-button">Toevoegen</x-submit>
+                            </form>
+                        </x-cardstripe>
                     </div>
                 @endforeach
-            @endif
-        </div>
-    </div>
-
-    <div class="card-containerproduc">
-        @foreach(\App\Models\Product::all()->where('type', '=', 'fruit') as $product)
-            <div class="mb-3">
-                <x-cardstripe :title="$product->naam" class="font-stylecard">
-                    <form action="/verpakkingInhoudToevoegen" method="post">
-                        @csrf
-                        <x-input type="hidden" name="selectie_id" :value="$selectie->id" />
-                        <x-input type="hidden" name="product_id" :value="$product->id" />
-                        <div class="form-length">
-                            <x-input name="aantal" label="aantal" />
-                        </div>
-                        <x-submit class="small-button">Toevoegen</x-submit>
-                    </form>
-                </x-cardstripe>
             </div>
-        @endforeach
+        </div>
+
+        <div class="col-4">
+            <x-cardstripe class="bo-hoofdkleur-opacity" title="Inhoud {{ $selectie->naam }}">
+                @if($inhoud !== null)
+                    <div class="inhoud-lijst">
+                        @foreach($inhoud as $item)
+                            @php $product = \App\Models\Product::find($item->product_id)@endphp
+                            <div class="inhoud-lijst-item d-flex justify-content-between">
+                                <div class="text-container">
+                                    <p>{{$product->naam}}</p>
+                                    <p>{{$item->aantal}}</p>
+                                </div>
+                                <div class="mb-2">
+                                    <form action="/deleteVerpakkingInhoud" method="post" class="d-inline-block">
+                                        @csrf
+                                        <input name="product_id" value="{{$product->id}}" type="hidden">
+                                        <input name="selectie_id" value="{{$selectie->id}}" type="hidden">
+                                        <x-submit class="smallest-button">Verwijder</x-submit>
+                                    </form>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
+            </x-cardstripe>
+        </div>
+
+
     </div>
-
-
-
 </x-layout>
