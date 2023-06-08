4c388da6f1f6a70d67c5d6e4aa00bb0fe801a7f1
dionpool <elirionn@gmail.com>
2023-06-08 10:27:16 +0200

    Voegt data vanuit database toe aan aardbei component
    Signed-off-by: dionpool <elirionn@gmail.com>

--- resources/views/components/aardbei.blade.php
+++ resources/views/components/aardbei.blade.php
@@ -4,22 +4,21 @@
     </button>
     <div class="dropup-content">
         <div class="accordion accordion-flush" id="faq">
-            <div class="accordion-item">
-                <h2 class="accordion-header">
-                    <button class="accordion-link collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#faq-content">
-                        Title
-                    </button>
-                </h2>
-                <div id="faq-content" class="accordion-collapse collapse" data-bs-parent="#faq">
-                    <div class="accordion-body">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores nesciunt nostrum
-                        officiis placeat possimus vero? Accusamus aliquid, assumenda consequatur delectus
-                        exercitationem
-                        laboriosam modi molestiae molestias officia recusandae repudiandae saepe.
+            @foreach(\App\Models\Faq::all() as $faq)
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-link collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#{{ $faq->id }}">
+                            {{ $faq->question }}
+                        </button>
+                    </h2>
+                    <div id="{{ $faq->id }}" class="accordion-collapse collapse" data-bs-parent="#faq">
+                        <div class="accordion-body">
+                            {{ $faq->answer }}
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </div>
