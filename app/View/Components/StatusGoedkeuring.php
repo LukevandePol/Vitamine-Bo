55fa0b54744c554fe652f51b94f7dc71372e61f3
dionpool <elirionn@gmail.com>
2023-06-28 13:27:45 +0200

    Voegt logica toe om bestelling status te bekijken
    Signed-off-by: dionpool <elirionn@gmail.com>

--- app/View/Components/StatusGoedkeuring.php
+++ app/View/Components/StatusGoedkeuring.php
@@ -2,6 +2,7 @@
 
 namespace App\View\Components;
 
+use App\Models\Bestelling;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
@@ -21,6 +22,8 @@ class StatusGoedkeuring extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.dashboard.status-goedkeuring');
+        $redenen = Bestelling::pluck('reden');
+
+        return view('components.dashboard.status-goedkeuring', compact('redenen'));
     }
 }
